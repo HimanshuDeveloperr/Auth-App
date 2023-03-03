a04d41ad8b93533dc10b4420ed1613f6df21dbd5
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-03-03 21:38:27 +0530

    conditionalRenderingAsPerContext MainNavigation

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../store/AuthContext';
 
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
+
+  const context=useContext(AuthContext)
+
+  const isLoggedIn=context.isLoggedIn
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,15 +16,22 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
+          {!isLoggedIn && 
           <li>
             <Link to='/auth'>Login</Link>
           </li>
+          }
+          {isLoggedIn && 
           <li>
             <Link to='/profile'>Profile</Link>
           </li>
+          }
+          
+          {isLoggedIn && 
           <li>
             <button>Logout</button>
           </li>
+          }
         </ul>
       </nav>
     </header>
