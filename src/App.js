6860c28e952efdb23ee8af07106e0bea816d0480
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-03-06 13:42:13 +0530

    conditional rendering of routes Usecontext IMP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import React ,{useContext}from 'react'
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/AuthContext';
+
+
 
 function App() {
+  const context=useContext(AuthContext)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
+        {
+          !context.isLoggedIn &&
         <Route path='/auth'>
           <AuthPage />
         </Route>
+        }
+        {
+          context.isLoggedIn &&
         <Route path='/profile'>
           <UserProfile />
+        </Route>
+        }
+        <Route path="*">
+          <Redirect to="/"/>
         </Route>
       </Switch>
     </Layout>
